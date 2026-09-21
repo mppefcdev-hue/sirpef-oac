a6b5a0b59e09f529f499591e7a344aefcefd46a5
mppefcdev-hue <mppefc.dev@gmail.com>
2026-09-21 11:29:04 -0400

    feat: ready dockerfile and delete pwa

--- sirpef_vue/vite.config.ts
+++ sirpef_vue/vite.config.ts
@@ -2,54 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   server: { 
-     host: '0.0.0.0',
-     port: 5173 
+  server: { 
+    host: '0.0.0.0',
+    port: 5173 
   },
   plugins: [
-    vue(),
-    VitePWA({
-      injectRegister: 'auto',
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'SIRPE App',
-        short_name: 'SIRPE',
-        description: 'SIRPE App description',
-        theme_color: '#ffffff',
-        icons: [{
-          src: 'pwa-64x64.png',
-          sizes: '64x64',
-          type: 'image/png'
-        }, {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        }, {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any'  
-        }, {
-          src: 'maskable-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable'
-        }]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      devOptions: {
-        enabled: true
-        /* other options */
-      } 
-    })
+    vue()
   ],
-resolve: {
+  resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
